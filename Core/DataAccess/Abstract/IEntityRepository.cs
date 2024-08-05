6cd099395e7811ba79fbd9d4d8e48f5b2bb62de4
Kemal Deniz Lağap <91020791+kemaldenizl@users.noreply.github.com>
2024-08-05 11:36:40 +0300

    Implementing Core layer

---
+++ Core/DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.Abstract
+{
+    public interface IEntityRepository<TEntity> where TEntity : class
+    {
+        List<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null);
+        TEntity Get(Expression<Func<TEntity, bool>> filter);
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
