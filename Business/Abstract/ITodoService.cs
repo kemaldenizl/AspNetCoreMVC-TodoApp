11746bb14e8a70ea2bb0be050d2162600c5e2a8c
Kemal Deniz Lağap <91020791+kemaldenizl@users.noreply.github.com>
2024-08-05 12:14:08 +0300

    Coding ITodoService

--- Business/Abstract/ITodoService.cs
+++ Business/Abstract/ITodoService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace Business.Abstract
 {
     public interface ITodoService
     {
+        List<Todo> GetAllTodos();
+        Todo GetTodo(int id);
+        List<Todo> GetUnComplatedTodos();
+        List<Todo> GetComplatedTodos();
+        void Add(Todo todo);
+        void Update(Todo todo);
+        void Delete(Todo todo);
     }
 }
