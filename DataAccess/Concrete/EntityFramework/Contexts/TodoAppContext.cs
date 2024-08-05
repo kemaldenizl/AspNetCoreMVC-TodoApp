fb148eb27ac63e134fcba3a2a6c3076ac642bd38
Kemal Deniz Lağap <91020791+kemaldenizl@users.noreply.github.com>
2024-08-05 11:54:45 +0300

    Initialize database with databasefirst technic creating Context and table entity

---
+++ DataAccess/Concrete/EntityFramework/Contexts/TodoAppContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts;
+
+public partial class TodoAppContext : DbContext
+{
+    public TodoAppContext()
+    {
+    }
+
+    public TodoAppContext(DbContextOptions<TodoAppContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Todo> Todos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("server=DESKTOP-A88KP21; database=todoApp; integrated security=SSPI; TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Entities/Concrete/Todo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Core.Entities.Abstract;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entities.Concrete;
+
+public partial class Todo : IEntity
+{
+    [Key]
+    public int Id { get; set; }
+
+    [StringLength(50)]
+    [Unicode(false)]
+    public string? Description { get; set; }
+
+    public DateOnly? CreateDate { get; set; }
+
+    public DateOnly? StartDate { get; set; }
+
+    public DateOnly? EndDate { get; set; }
+
+    public bool? IsComplated { get; set; }
+}
