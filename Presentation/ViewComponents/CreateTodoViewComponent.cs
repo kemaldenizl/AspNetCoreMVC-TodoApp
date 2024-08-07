a01341645e70f6e4a02b8e2850be78ee0ea3c63f
Kemal Deniz Lağap <91020791+kemaldenizl@users.noreply.github.com>
2024-08-07 13:23:45 +0300

    Define CreateTodoViewComponent Invoke method  and View

--- Presentation/ViewComponents/CreateTodoViewComponent.cs
+++ Presentation/ViewComponents/CreateTodoViewComponent.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
 
 namespace Presentation.ViewComponents
 {
     public class CreateTodoViewComponent : ViewComponent
     {
+        public ViewViewComponentResult Invoke()
+        {
+            return View();
+        }
     }
 }
