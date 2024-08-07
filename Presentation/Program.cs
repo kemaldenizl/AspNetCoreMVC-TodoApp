2ebe5319c475f5462c081cfd34f87f82e91d1025
Kemal Deniz Lağap <91020791+kemaldenizl@users.noreply.github.com>
2024-08-07 12:51:29 +0300

    refactoring program.cs for changes

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -28,7 +28,7 @@ namespace Presentation
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Todo}/{action=Index}/{id?}");
 
             app.Run();
         }
