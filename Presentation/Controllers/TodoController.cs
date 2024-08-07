fc7ac4b8b717eb5f292d5a8ecd901e264fbdcce1
Kemal Deniz Lağap <91020791+kemaldenizl@users.noreply.github.com>
2024-08-07 12:49:36 +0300

    Creating TodoController and View page for index action

---
+++ Presentation/Controllers/TodoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
