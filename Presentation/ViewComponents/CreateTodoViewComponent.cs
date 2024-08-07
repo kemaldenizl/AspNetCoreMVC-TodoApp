341c487e7abfcdb4589dc58bf389d776b80b3854
Kemal Deniz Lağap <91020791+kemaldenizl@users.noreply.github.com>
2024-08-07 15:26:44 +0300

    Adding TodoService with dependecy injection for CreateTodoViewComponent

--- Presentation/ViewComponents/CreateTodoViewComponent.cs
+++ Presentation/ViewComponents/CreateTodoViewComponent.cs
@@ -1,13 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Presentation.Models;
 
 namespace Presentation.ViewComponents
 {
     public class CreateTodoViewComponent : ViewComponent
     {
+        private readonly ITodoService _todoService;
+        public CreateTodoViewComponent(ITodoService todoService)
+        {
+            _todoService = todoService;
+        }
         public ViewViewComponentResult Invoke()
         {
             return View();
         }
+
+        
     }
 }
